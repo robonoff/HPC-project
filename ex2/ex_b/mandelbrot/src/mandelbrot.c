f808d6b882668bb8d09e2fe82ea2e9c17a6da4c7
Roberta Lamberti <rlamberti@login01.hpc.rd.areasciencepark.it>
2025-07-23 17:39:33 +0200

    added data

--- ex2/ex_b/mandelbrot/src/mandelbrot.c
+++ ex2/ex_b/mandelbrot/src/mandelbrot.c
@@ -17,35 +17,18 @@ void compute_mandelbrot(void *M, int nx, int ny,
                         double xL, double yL,
                         double dx, double dy,
                         int I_max, int is_16bit) {
+    
+    #pragma omp parallel for collapse(2) schedule(dynamic, 16)
+    for (int j = 0; j < ny; j++) {
+        for (int i = 0; i < nx; i++) {
+            double cx = xL + i * dx;
+            double cy = yL + j * dy;
+            int val = iterate(cx, cy, I_max);
 
-    #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            #pragma omp taskgroup
-            {
-                for (int jj = 0; jj < ny; jj += 32) {
-                    for (int ii = 0; ii < nx; ii += 32) {
-                        int i0 = ii, j0 = jj;
-                        int i1 = (ii + 32 < nx) ? ii + 32 : nx;
-                        int j1 = (jj + 32 < ny) ? jj + 32 : ny;
-
-                        #pragma omp task firstprivate(i0, i1, j0, j1) shared(M)
-                        for (int j = j0; j < j1; j++) {
-                            #pragma omp taskloop grainsize(4) nogroup
-                            for (int i = i0; i < i1; i++) {
-                                double cx = xL + i * dx;
-                                double cy = yL + j * dy;
-                                int val = iterate(cx, cy, I_max);
-                                if (is_16bit)
-                                    ((ushort*)M)[j * nx + i] = (val == I_max) ? 0 : val;
-                                else
-                                    ((byte*)M)[j * nx + i] = (val == I_max) ? 0 : val;
-                            }
-                        }
-                    }
-                }
-            }
+            if (is_16bit)
+                ((ushort*)M)[j * nx + i] = (val == I_max) ? 0 : val;
+            else
+                ((byte*)M)[j * nx + i] = (val == I_max) ? 0 : val;
         }
     }
 }
